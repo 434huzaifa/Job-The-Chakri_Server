f4e14a7258cd04f5c84c94bdcf786c70c8dc11b7
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2023-11-09 07:20:27 +0600

    user post, get api

--- index.js
+++ index.js
@@ -3,37 +3,51 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const app = express()
-const port=process.env.PORT || 54321
+const port = process.env.PORT || 54321
 
 app.use(cors());
-app.use(express.json())
-
-const uri=`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@saaddb.bmj48ga.mongodb.net/?retryWrites=true&w=majority`
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@saaddb.bmj48ga.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: false,
-      deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: false,
+    deprecationErrors: true,
+  }
 });
 async function run() {
-    try {
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  
-    } finally {
-        await client.close();
-    }
+
+  try {
+    const database = client.db("JobTheChakri");
+    const cbid = database.collection("bid");
+    const cjobs = database.collection("jobs");
+    const cuser = database.collection("user");
+    app.post('/user', async (req, res) => {
+      const user = req.body
+      const query = { email: new RegExp(user.email, "i") }
+      const userArray=await cuser.findOne(query)
+      if (userArray==null) {
+        const result = await cuser.insertOne(user)
+        res.send(result)
+        return
+      }
+      res.send({msg:"user exist"})
+    })
+    app.get('/top',async(req,res)=>{
+      const users=await cuser.find().limit(5).toArray()
+      res.send(users)
+    })
+
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 
-app.get('/',(req, res) => {res.send("Backend Running")})
+app.get('/', (req, res) => { res.send("Backend Running") })
 
 
-app.listen(port, () => { console.log(`Server Started`) })
+app.listen(port, () => { console.log(`Server Started at ${port}`) })
 
 
